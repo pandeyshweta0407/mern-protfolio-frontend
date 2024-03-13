42515f2899944e967852a6b9e28db042df883440
pandeyShweta0407 <shwetapandey60023@gmail.com>
2024-03-13 11:44:30 +0530

    second commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { FaLinkedin } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
 function Contact() {
-
   return (   
     <section className="contact" id="Contact">
       <h2 className="contact-title">Contact Me</h2>
@@ -10,14 +10,13 @@ function Contact() {
       <div className="contact-left">
           <h4 className="contact-text">let's work together</h4>
           <div className="contact-list">
-            <a href="#"><i className="fa-regular fa-envelope "></i>shwetapandey60023@gmail.com</a>
-            <a href="#"><i className="fa-brands fa-github"></i>https://github.com/pandeyshweta0407</a>
-            <a href="#"><i className="fa-brands fa-linkedin"></i>www.linkedin.com/in/pandeyshweta0407</a>
+            <a href="https://github.com/pandeyshweta0407"><FaGithub className='text-4xl' /></a>
+            <a href="shwetapandey60023@gmail.com"><MdEmail className='text-4xl' /></a>
+            <a href="www.linkedin.com/in/pandeyshweta0407"><FaLinkedin className='text-4xl' /></a>
           </div>
         </div>
 
         <div className="contact-right"  id="contact-right">
-          <h1 id="msg"></h1>
           <form>
           <input type="text" name="name" placeholder="Enter your name" id="" required/>
             <input type="text" name="email" placeholder="Enter your email" id="" required/>
