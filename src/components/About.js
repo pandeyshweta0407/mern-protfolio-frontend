45b059ff537ae470a0960a695468ecc1074fb1d5
pandeyShweta0407 <shwetapandey60023@gmail.com>
2024-03-13 15:41:05 +0530

    fouth commit

--- src/components/About.js
+++ src/components/About.js
@@ -6,22 +6,40 @@ function About() {
 
 
 <div className="about" id="About">
-    <h2 class="about-topic">About me</h2>  
-    <img  class="about-img" src={AboutImg} alt="about" /> 
+      <div className="about-topic" >
+      <h2 >About me</h2> 
+      </div> 
+
+      <div className="about-content" >
+       <div className ="about-img" >
+    <img   src={AboutImg} alt="about" /> 
+    </div>
       <div className="about-text">
         I'm a 4th-year student at MERI College, currently pursuing my Btech (CSE)
         <br/>
-        Passionate about technology and software development, I am on a journey to enhance my skills and knowledge in this field.
         <br/>
-        It's been few years in programming world i learnt about java and Data Structure and Algorithm .And now I am learning about web development and recently built few projects on MERN STACK and ASP.NET CORE MVC .
+        Passionate about technology and software development, 
+        <br/>I am on a journey to enhance my skills and knowledge in this field.
         <br/>
-        I am eager to continue learning and expanding my skill set, with a keen interest in exploring more programming. languages and frameworks to become a proficient and versatile developer.
         <br/>
-        I am open to new opportunities,collaborations, and experiences that allow me to apply my knowledge and contribute to exciting projects.
+        It's been few years in programming world i learnt about java and Data 
+        <br/>Structure and Algorithm .And now I am learning about web development 
+        <br/>and recently built few projects on MERN STACK and ASP.NET CORE MVC .
+        <br/>
+        <br/>
+        I am eager to continue learning and expanding my skill set,
+        <br/> with a keen interest in exploring more programming. 
+        <br/> languages and frameworks to become a proficient and versatile developer.
+        <br/>
+        <br/>
+        I am open to new opportunities,collaborations, and experiences 
+        <br/> that allow me to apply my knowledge and contribute to exciting projects.
+        <br/>
         <br/>
         Let's connect and grow together in the dynamic realm of technology!
       
       </div>
+       </div>
     </div>
 
 
