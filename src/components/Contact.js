c533777aae3097275059680d801555e81734baf0
pandeyShweta0407 <shwetapandey60023@gmail.com>
2024-03-14 16:44:35 +0530

    fifth commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaLinkedin } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { baseURL } from '../utils/APIRoutes';
+
+
 function Contact() {
+
+   const [inputValue , setInputValue ] = useState({
+       name :"", 
+       email : "",
+       phone : "",
+       subject : "",
+       message :"",
+   })  
+   console.log(inputValue);
+
+   const getValue = (e) =>{
+    console.log(e.target.value);
+    const {name , value} = e.target;
+    setInputValue(()=>{
+       return {
+        ...inputValue , [name] : value
+       }
+    })
+   }
+   const toastOptions = {
+    position: "top-right",
+    autoClose: 10000,
+    pauseOnHover: true,
+    theme: "light",
+    closeOnClick: true,
+    draggable: true,
+  };
+
+    const sentUserData = async (e)=>{
+      e.preventDefault();
+      const {name , email, phone , subject , message} = inputValue;
+      const res = await fetch(`${baseURL}/addmsg` ,{
+        method: "POST",
+        headers:{
+          "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({
+          name , email, phone , subject , message
+        })
+      }) ;
+        const data = res.json();
+        const status = data.status;
+        if(status === false ){
+          console.log(status);
+        }else{
+          console.log(status);
+          toast("messsage send successfully" ,  toastOptions);
+        }
+    }
+
+
   return (   
     <section className="contact" id="Contact">
       <h2 className="contact-title">Contact Me</h2>
@@ -17,13 +73,14 @@ function Contact() {
         </div>
 
         <div className="contact-right"  id="contact-right">
+        <ToastContainer />
           <form>
-          <input type="text" name="name" placeholder="Enter your name" id="" required/>
-            <input type="text" name="email" placeholder="Enter your email" id="" required/>
-            <input type="text" name="phone" placeholder="Enter your phone" id="" required/>
-            <input type="text" name="subject" placeholder="Enter your Subject" id="" required/>
-            <textarea name="message" placeholder="Enter your message" id="" cols="30" rows="4"></textarea>
-            <input type="submit" value="submit" class="send" id="send"/> 
+          <input type="text" name="name" placeholder="Enter your name" id="" required          onChange={getValue}  />
+            <input type="email" name="email" placeholder="Enter your email" id="" required     onChange={getValue}  />
+            <input type="text" name="phone" placeholder="Enter your phone" id="" required      onChange={getValue}  />
+            <input type="text" name="subject" placeholder="Enter your Subject" id="" required  onChange={getValue}  />
+            <textarea name="message" placeholder="Enter your message" id="" cols="30" rows="4" onChange={getValue}  ></textarea>
+            <input type="submit" value="submit" className ="send" id="send" onClick={sentUserData}  /> 
             </form>
         </div>
 
---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,6 @@
+// export const baseURL = "http://localhost:5000"
+
+
+      
+export const baseURL = "https://mern-protfolio-backend.onrender.com"
+
